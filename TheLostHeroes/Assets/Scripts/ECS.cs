581346e4ab12b5e68fdc5cd31c777e182f480963
Kiryanov Grigory <40793299+SingularGamesStudio@users.noreply.github.com>
2024-12-06 16:20:47 +0300

    ECS setup

---
+++ TheLostHeroes/Assets/Scripts/ECS.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leopotam.Ecs;
+
+public class ECS : MonoBehaviour
+{
+    public StaticData configuration;
+    public SceneData sceneData;
+    private EcsWorld ecsWorld;
+    private EcsSystems systems;
+
+    private void Start()
+    {
+        ecsWorld = new EcsWorld();
+        systems = new EcsSystems(ecsWorld);
+        RuntimeData runtimeData = new RuntimeData();
+
+        systems
+            .Add(new WorldInitSystem())
+            .Inject(configuration)
+            .Inject(sceneData)
+            .Inject(runtimeData)
+            .Init();
+    }
+
+
+    private void Update()
+    {
+        systems?.Run();
+    }
+
+    private void OnDestroy()
+    {
+        systems?.Destroy();
+        systems = null;
+        ecsWorld?.Destroy();
+        ecsWorld = null;
+    }
+}
---
+++ TheLostHeroes/Assets/Scripts/RuntimeData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeData
+{
+    //stuff only created in runtime
+}
