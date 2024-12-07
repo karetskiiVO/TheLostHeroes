27edb29407bb3eab6ca66942c613d7f28a0b614c
vladislav karetskij <karetskii.vo@phystech.edu>
2024-12-07 14:26:24 +0300

    initial

--- TheLostHeroes/Assets/Scripts/Systems/WorldGenSystem.cs
+++ TheLostHeroes/Assets/Scripts/Systems/WorldGenSystem.cs
@@ -6,11 +6,17 @@ using UnityEngine.AI;
 
 public struct WorldGenSystem : IEcsInitSystem
 {
-    private EcsWorld ecsWorld; //подтягивается автоматически, так как наследует EcsWorld
-    private StaticData staticData;//подтягивается из Inject
-    private RuntimeData runtimeData;//подтягивается из Inject
-    public void Init()
-    {
-        //Generate world using stuff in runtimeData
+    private EcsWorld ecsWorld;          // подтягивается автоматически, так как наследует EcsWorld
+    private StaticData staticData;      // подтягивается из Inject
+    private RuntimeData runtimeData;    // подтягивается из Inject
+
+    public EcsFilter<Map> filter;
+
+    public void Init() {
+        foreach (var i in filter) {
+            ref EcsEntity mapEntity = ref filter.GetEntity(i);
+
+            // алгоритм генерации
+        }
     }
 }
