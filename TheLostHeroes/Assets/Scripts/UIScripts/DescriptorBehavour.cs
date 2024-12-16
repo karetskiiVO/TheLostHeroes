b653ea8e370ad020472cf0c19a7361fdf57411ba
vladislav karetskij <karetskii.vo@phystech.edu>
2024-12-17 01:49:53 +0300

    started with gameplay UI

---
+++ TheLostHeroes/Assets/Scripts/UIScripts/DescriptorBehavour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DescriptorBehavour : MonoBehaviour {
+    [SerializeField]private Text entityName;
+    [SerializeField]private Text entityDescription;
+
+    private void Start () {
+        SetDefoltDescription();
+    }
+
+    public void SetDesctription (string name, string description) {
+        entityName.text = name;
+        entityDescription.text = description;
+    }
+
+    public void SetDefoltDescription () {
+        SetDesctription("<EntityName>", "<EntityDescription>");
+    }
+}
