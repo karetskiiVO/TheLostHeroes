1e57b51e2131879a64378d4f8f7b83e68004d7b0
vladislav karetskij <karetskii.vo@phystech.edu>
2024-12-22 01:44:21 +0300

    новая система активации

--- TheLostHeroes/Assets/Scripts/Systems/MouseClickSystem.cs
+++ TheLostHeroes/Assets/Scripts/Systems/MouseClickSystem.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 using Leopotam.Ecs;
 
 using Photon.Pun;
-public struct MouseClickSystem : IEcsRunSystem
+using UnityEngine.UI;
+public struct MouseClickSystem : IEcsRunSystem, IEcsInitSystem
 {
     private EcsWorld ecsWorld;          // подтягивается автоматически, так как наследует EcsWorld
     private StaticData staticData;      // подтягивается из Inject
 
     private RuntimeData runtimeData;    // подтягивается из Inject
 
-    EcsFilter<Pawn> pawnFilter;
+    private Button backgroundButton;
+    private class BoolClass
+    {
+        public bool val = false;
+
+        public void Activate () {
+            val = true;
+        }
+    }
+    private BoolClass clickedFlag;
+
+    public void Init()
+    {
+        clickedFlag = new BoolClass();
+
+        var backgroundButtonGameObject = GameObject.Find("Canvas");
+
+        var img = backgroundButtonGameObject.AddComponent<Image>();
+        img.color = new Color(1, 1, 1, 0); 
+        var transform = backgroundButtonGameObject.GetComponent<RectTransform>();
+        transform.sizeDelta = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+        transform.anchoredPosition = Vector2.zero;
+        backgroundButton = backgroundButtonGameObject.AddComponent<Button>();
+
+        backgroundButton.onClick.AddListener(clickedFlag.Activate);
+    }
 
     public void Run()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        if (clickedFlag.val)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
@@ -29,5 +55,7 @@ public struct MouseClickSystem : IEcsRunSystem
 
             clicked?.OnClick();
         }
+
+        clickedFlag.val = false;
     }
 }
